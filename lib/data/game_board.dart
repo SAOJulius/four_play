213246344c18d7d391deb29eee5f11dd8416c89e
Julius <julius.krams@gmail.com>
2023-11-06 16:42:42 +0100

    Hizufügen von moves ins spiel

--- lib/data/game_board.dart
+++ lib/data/game_board.dart
@@ -1,42 +1,88 @@
+import 'dart:math';
 
 class GameBoard {
   Map<String, bool> fieldOccupancy = {};
 
-  // Methode zum Hinzufügen eines belegten Feldes mit den Koordinaten (x, y).
-  void occupyField(int x, int y) {
-    fieldOccupancy['$x,$y'] = true;
-  }
-
-  // Methode zum Überprüfen, ob ein Feld mit den Koordinaten (x, y) belegt ist.
-  bool isFieldOccupied(int x, int y) {
-    return fieldOccupancy['$x,$y'] ?? false;
-  }
-
-  // Methode zum Überprüfen und Belegen der Felder in einer Reihe.
-  List<String> checkAndOccupyRow(int startX, int startY) {
-    List<String> occupiedCoordinates = [];
-    int x = startX;
-    int y = startY;
-    int step = 1;
-
-    while (y >= 0) {
-      if (!isFieldOccupied(x, y)) {
-        occupyField(x, y);
-        occupiedCoordinates.add('$x,$y');
-      } else {
-        // Markiere das vorherige Feld als belegt und bewege dich einen Schritt weiter.
-        occupyField(x - step, y);
-        x += step;
+  GameBoard(int radius) {
+    // Initialisiere das fieldOccupancy-Dictionary entsprechend dem hexagonalen Raster.
+    for (int q = -radius; q <= radius; q++) {
+      int r1 = max(-radius, -q - radius);
+      int r2 = min(radius, -q + radius);
+      for (int r = r1; r <= r2; r++) {
+        fieldOccupancy['$q,$r'] = false;
       }
-
-      if (x == 0) {
-        // Ändere die Richtung, wenn x den Nullpunkt erreicht hat.
-        step = -step;
-      }
-      y++;
     }
+  }
 
-    return occupiedCoordinates;
+  List<int> assignAvailableCoordinateInRow(List<int> startingCoordinates) {
+      int q = startingCoordinates[0];
+      int r = startingCoordinates[1];
+
+      if (q == 3 && r == -3) {
+        for (int i = 0; i >= r; i--){
+            if (isFieldOccupied(q, i) == false) {
+              occupyField(q, i);
+              return [q, i];
+            } else {
+
+            }
+        }
+      } else if (q == -3 && r == 0) {
+        for (int i = 3; i >= r; i--){
+          if (isFieldOccupied(q, i) == false) {
+            occupyField(q, i);
+            return [q, i];
+          }
+        }
+      } else if (q == -2 && r == -1) {
+        for (int i = 3; i >= r; i--){
+          if (isFieldOccupied(q, i) == false) {
+            occupyField(q, i);
+            return [q, i];
+          }
+        }
+      } else if (q == 2 && r == -3) {
+        for (int i = 1; i >= r; i--){
+          if (isFieldOccupied(q, i) == false) {
+            occupyField(q, i);
+            return [q, i];
+          }
+        }
+      } else if (q == -1 && r == -2) {
+        for (int i = 3; i >= r; i--){
+          if (isFieldOccupied(q, i) == false) {
+            occupyField(q, i);
+            return [q, i];
+          }
+        }
+      } else if (q == 1 && r == -3) {
+        for (int i = 2; i >= r; i--){
+          if (isFieldOccupied(q, i) == false) {
+            occupyField(q, i);
+            return [q, i];
+          }
+        }
+      } else if (q == 0 && r == -3) {
+        for (int i = 3; i >= r; i--){
+          if (isFieldOccupied(q, i) == false) {
+            occupyField(q, i);
+            return [q, i];
+          }
+        }
+      }
+
+    return [];
+  }
+
+  // Methode zum Prüfen, ob ein Feld mit den Koordinaten (q, r) belegt ist.
+  bool isFieldOccupied(int q, int r) {
+    String coordinateKey = '$q,$r';
+    return fieldOccupancy[coordinateKey] ?? false;
+  }
+
+  // Methode zum Belegen eines Feldes mit den Koordinaten (q, r).
+  void occupyField(int q, int r) {
+    String coordinateKey = '$q,$r';
+    fieldOccupancy[coordinateKey] = true;
   }
 }
-
--- lib/ui/widgets/game_board_widget.dart
+++ lib/ui/widgets/game_board_widget.dart
@@ -9,7 +9,7 @@ import '../../data/game_board.dart';
 class HexagonalGameBoard extends StatelessWidget {
   final HexagonType type = HexagonType.FLAT;
   final int depth = 3;
-  late GameBoard gameBoard;
+  GameBoard gameBoard = GameBoard(3);
 
   @override
   Widget build(BuildContext context) {
@@ -36,30 +36,9 @@ class HexagonalGameBoard extends StatelessWidget {
               child: InkWell(
                 onTap: () async {
                   // Ändere die Farbe nur beim Klicken
-                  if (coordinates.q == 0 && coordinates.r == -3) {
-                    print(coordinates);
-                    GameBoard gameBoard = GameBoard();
-                    List<String> test = gameBoard.checkAndOccupyRow(0, -3);
-                    print(test);
-                    try {
-                      hexagonGridProvider.changeHexagonColor(
-                          test[0] as int, test[0] as int, Colors.red);
-                    } catch (e) {
-                      hexagonGridProvider.changeHexagonColor(
-                          0, 3, Colors.red);
-                    }
-                    //for (int i = 0, value1 = -4, value2 = -3; i < 7; i++) {
-                      //await Future.delayed(Duration(milliseconds: 30));
-                      //hexagonGridProvider.changeHexagonColor(0, value1, Colors.white);
-                      //hexagonGridProvider.changeHexagonColor(0, value2, Colors.red);
-                      //value1++;
-                      //value2++;
-                    //}
-
-                  } else {
-                    hexagonGridProvider.changeHexagonColor(
-                        coordinates.q, coordinates.r, Colors.red);
-                  }
+                  List<int> test = gameBoard.assignAvailableCoordinateInRow([coordinates.q, coordinates.r]);
+                  hexagonGridProvider.changeHexagonColor(
+                      test[0],test[1], Colors.blue);
                 },
                 child: Column(
                   mainAxisAlignment: MainAxisAlignment.center,
