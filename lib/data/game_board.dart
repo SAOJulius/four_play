fc01b12dfaac4f73a340b8c284bc1f9eeb96fbd5
SAOJulius <Julius.krams@gmail.com>
2023-11-06 10:33:15 +0100

    Anfang hinzufügen funtionalität des game_board_widget.dart

--- lib/data/game_board.dart
+++ lib/data/game_board.dart
@@ -1,7 +1,42 @@
-import 'package:flutter/material.dart';
 
-class AppColors {
+class GameBoard {
+  Map<String, bool> fieldOccupancy = {};
 
+  // Methode zum Hinzufügen eines belegten Feldes mit den Koordinaten (x, y).
+  void occupyField(int x, int y) {
+    fieldOccupancy['$x,$y'] = true;
+  }
 
+  // Methode zum Überprüfen, ob ein Feld mit den Koordinaten (x, y) belegt ist.
+  bool isFieldOccupied(int x, int y) {
+    return fieldOccupancy['$x,$y'] ?? false;
+  }
 
+  // Methode zum Überprüfen und Belegen der Felder in einer Reihe.
+  List<String> checkAndOccupyRow(int startX, int startY) {
+    List<String> occupiedCoordinates = [];
+    int x = startX;
+    int y = startY;
+    int step = 1;
+
+    while (y >= 0) {
+      if (!isFieldOccupied(x, y)) {
+        occupyField(x, y);
+        occupiedCoordinates.add('$x,$y');
+      } else {
+        // Markiere das vorherige Feld als belegt und bewege dich einen Schritt weiter.
+        occupyField(x - step, y);
+        x += step;
+      }
+
+      if (x == 0) {
+        // Ändere die Richtung, wenn x den Nullpunkt erreicht hat.
+        step = -step;
+      }
+      y++;
+    }
+
+    return occupiedCoordinates;
+  }
 }
+
--- lib/ui/widgets/game_board_widget.dart
+++ lib/ui/widgets/game_board_widget.dart
@@ -3,10 +3,13 @@ import 'package:hexagon/hexagon.dart';
 import 'package:provider/provider.dart';
 
 import '../../colors.dart';
+import '../../data/game_board.dart';
+import '../../data/game_board.dart';
 
 class HexagonalGameBoard extends StatelessWidget {
   final HexagonType type = HexagonType.FLAT;
   final int depth = 3;
+  late GameBoard gameBoard;
 
   @override
   Widget build(BuildContext context) {
@@ -31,11 +34,28 @@ class HexagonalGameBoard extends StatelessWidget {
               padding: 2.0,
               cornerRadius: 8.0,
               child: InkWell(
-                onTap: () {
+                onTap: () async {
                   // Ändere die Farbe nur beim Klicken
                   if (coordinates.q == 0 && coordinates.r == -3) {
-                    hexagonGridProvider.changeHexagonColor(
-                        0, 3, Colors.red);
+                    print(coordinates);
+                    GameBoard gameBoard = GameBoard();
+                    List<String> test = gameBoard.checkAndOccupyRow(0, -3);
+                    print(test);
+                    try {
+                      hexagonGridProvider.changeHexagonColor(
+                          test[0] as int, test[0] as int, Colors.red);
+                    } catch (e) {
+                      hexagonGridProvider.changeHexagonColor(
+                          0, 3, Colors.red);
+                    }
+                    //for (int i = 0, value1 = -4, value2 = -3; i < 7; i++) {
+                      //await Future.delayed(Duration(milliseconds: 30));
+                      //hexagonGridProvider.changeHexagonColor(0, value1, Colors.white);
+                      //hexagonGridProvider.changeHexagonColor(0, value2, Colors.red);
+                      //value1++;
+                      //value2++;
+                    //}
+
                   } else {
                     hexagonGridProvider.changeHexagonColor(
                         coordinates.q, coordinates.r, Colors.red);
